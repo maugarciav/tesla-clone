a89afcf457e3d965545fbbe9eba9504344d2d18e
maugarciav <mau.villanueva2002@gmail.com>
2023-12-19 21:55:27 -0600

    add: link -> qorking on navbar onhover (yarn is crashing)

--- src/components/Model.js
+++ src/components/Model.js
@@ -6,13 +6,13 @@ const Model = () => {
       <div className='absolute inset-x-0  top-[15%] text-center text-[#171a20]'>
         <h1 className='text-4xl font-extrabold m-2'>Model S</h1>
         <p className='text-xl font-bold'>From $71,090*</p>
-        <p style={{ fontSize: '12px', fontWeight: 100 }}>After Est. Gas Savings</p>
+        <p className='text-[12px]'>After Est. Gas Savings</p>
       </div>
       <div className='flex flex-col items-center justify-center text-sm lg:flex-row absolute inset-x-0 bottom-[10%]'>
         <button className='bg-white opacity-65  rounded-sm w-80 lg:w-56 lg:mx-3 h-9 mt-2 text-xs font-semibold'>Explore Inventory</button>
         <button className='bg-[#393c41] opacity-65  rounded-sm w-80 lg:w-56 lg:mx-3 h-9 mt-2 text-white text-xs font-semibold'>Demo Drive</button>
       </div>
-      <div style={{ fontSize: '12px', fontWeight: 100 }} className='text-center justify-center absolute inset-x-0 bottom-[3%]'>
+      <div className='text-[12px] text-center justify-center absolute inset-x-0 bottom-[3%]'>
         <p>*Price before savings is $74,990, excluding taxes and fees. Subject to change.</p>
         <p className='underline underline-offset-4'>Learn about est. gas savings.</p>
       </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,21 @@
 import React from "react";
 import TeslaLogo from "../assets/TeslaLogo";
 
-
 const NavBar = () => {
   return (
-    <div className= "flex justify-between items-center p-4 px-8 text-xs font-semibold">
+    <div className="flex justify-between items-center p-4 px-8 text-xs font-semibold">
       <div>
-        <TeslaLogo/>
+        <a href="https://tesla.com">
+          <TeslaLogo />
+        </a>
       </div>
       <div className="hidden lg:inline">
         <ul className="flex justify-center hover:cursor-pointer">
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">Vehicles</li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">Energy</li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">Charging</li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">Discover</li>
-          <li className="py-1 px-3 hover:rounded hover:bg-black/5">Shop</li>
+          <SideBarIcon icon={"Vehicles"} />
+          <SideBarIcon icon={"Energy"} />
+          <SideBarIcon icon={"Charging"} />
+          <SideBarIcon icon={"Discover"} />
+          <SideBarIcon icon={"Shop"} />
         </ul>
       </div>
 
@@ -27,12 +28,23 @@ const NavBar = () => {
       </div>
 
       <div className="lg:hidden">
-        <button className= "inline-flex item-center roaded-md py-2 px-4 text-sm font-medium bg-black/5 shadow-sm hover:bg-black/10">Menu</button>
+        <button className="inline-flex item-center roaded-md py-2 px-4 text-sm font-medium bg-black/5 shadow-sm hover:bg-black/10">
+          Menu
+        </button>
       </div>
-
-
     </div>
   );
 };
+
+const SideBarIcon = ({ icon, text = "tooltip" }) => (
+  <>
+    <li className="py-1 px-3 hover:rounded hover:bg-black/5 group">
+      {icon}
+    <div className="absolute inset-0 bottom-[40%] scale-0 shadow-lg bg-white duration-500 origin-top  group-hover:scale-100">
+      {icon}
+    </div>
+    </li>
+  </>
+);
 
 export default NavBar;
